c907f3143581673ed3fa444d0232432d27ab08df
Donal McMullan <donal.mcmullan@bouldermedia.tv>
2019-07-03 19:15:39 +0100

    local env mods

--- bin/tooling/update_environment.sh
+++ bin/tooling/update_environment.sh
@@ -6,3 +6,8 @@ export CUEBOT_HOSTNAME="localhost"
 export CUE_FS_ROOT=/var/opencue
 export PYTHONDONTWRITEBYTECODE=1
 export OPENCUE_CONF="${BASEDIR}/bin/tooling/opencue_conf.yaml"
+
+local_mods="${BASEDIR}/bin/tooling/local_environment_mods.sh"
+if [ -e "${local_mods}" ]; then
+    source "${local_mods}"
+fi
